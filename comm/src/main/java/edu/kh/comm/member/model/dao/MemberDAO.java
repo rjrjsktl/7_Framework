868c34b5eda36eb13b55dd8a189e657ad11143df
YUN <rjrjsktl@gmail.com>
2023-04-25 21:46:08 +0900

    쿠키 로그인 로그아웃 등 기능 구현

--- comm/src/main/java/edu/kh/comm/member/model/dao/MemberDAO.java
+++ comm/src/main/java/edu/kh/comm/member/model/dao/MemberDAO.java
@@ -31,11 +31,19 @@ public class MemberDAO {
 //		logger.debug(count + "");
 		
 		// 1행 조회(파라미터 o) 방법
-		String memberNickname = sqlSession.selectOne("memberMapper.test2", inputMember.getMemberEmail());
-		logger.debug(memberNickname);
+		// String memberNickname = sqlSession.selectOne("memberMapper.test2", inputMember.getMemberEmail());
+		// logger.debug(memberNickname);
+		
+		// 1행 조회(파라미터 VO 경우)
+		// String memberTel = sqlSession.selectOne("memberMapper.test3", inputMember);
+																	// memberEmail, memberPw
+		// logger.debug(memberTel);
 		
 		
-		return null;
+		// 1행 조회(파라미터 VO, 반환되는 결과 VO) 
+		Member loginMember = sqlSession.selectOne("memberMapper.login", inputMember);
+		
+		return loginMember;
 	}
 
 }
